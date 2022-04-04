31ed91737f477b8b0a36cdf9415172b1031666b7
Philippe Ngom <philngom@gmail.com>
2022-04-04 10:45:24 -0700

    completed oddishOrEvenish

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function reverseWords(sentence) {
 }
 
 console.log(reverseWords('Took my dog for a walk'));
+
+function oddishOrEvenish(number) {
+  let total = 0;
+  // const numberArr = number.split('');
+
+  const numberStr = number + '';
+  numberStr.split('').map((number) => total += +number);
+
+  return total % 2 === 0 ? 'Evenish' : 'Oddish';
+}
+
+console.log(oddishOrEvenish(121));
